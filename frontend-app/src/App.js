398d61c37934795ab32afe998639140d7b275f6c
MarcosIraola <iraolamarcos@gmail.com>
2020-08-03 20:04:05 +0200

    Componente RoomCard

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RoomCard from "./components/roomCard/roomCard.view";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div>
+      <RoomCard/>
     </div>
   );
 }
