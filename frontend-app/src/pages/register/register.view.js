89c4bee1e05105385a577044f450ec0bcd6a71d5
facundoherrera <facundoh1986@gmail.com>
2020-09-10 10:26:44 +0200

    Formulario de registro completo

--- frontend-app/src/pages/register/register.view.js
+++ frontend-app/src/pages/register/register.view.js
@@ -1,24 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './register.module.css';
 import { Link } from 'react-router-dom';
 
 
 const RegisterForm = () => {
 
+    const [data, setData] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        email: '',
+        password: '',
+        role: 'HOST',
+
+
+    })
+
+
+    const handleInputChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const SubmitForm = () => {
+        const url = 'http://localhost/api/users';
+        const body = {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email,
+            username: data.username,
+            password: data.password,
+            role: data.role,
+
+
+        };
+        const options = {
+            method: 'POST',
+            headers: new Headers({
+                'Content-type': 'application/json',
+            }),
+            mode: 'cors',
+            body: JSON.stringify(body),
+        };
+
+        fetch(url, options)
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response.status);
+                    return response.json();
+                }
+                return Promise.reject(response.status);
+            })
+
+            .catch(error => console.log(error));
+    };
+
+    console.log(data);
     return (
        <>
            <div className={styles.__login_container}>
                <div className={styles.__login_div}>
                    <h3>Registrate</h3>
                    <br></br>
-                   <h6>¿Aún no tienes una cuenta? Registrarme</h6>
+                   <h6>¿Aún no tienes una cuenta? Registrate</h6>
                    <div className={styles.__login_form_div}>
-                       <input className={styles.__login_input} placeholder="Nombre "></input>
-                       <input className={styles.__login_input} placeholder="Apellido "></input>
-                       <input className={styles.__login_input} placeholder="E-mail "></input>
-                       <input className={styles.__login_input} placeholder="Contraseña "></input>
+                       <input className={styles.__login_input} name="first_name" type={'text'} placeholder="Nombre " onChange={handleInputChange}></input>
+                       <input className={styles.__login_input} name="last_name" type={'text'}placeholder="Apellido "onChange={handleInputChange}></input>
+                       <input className={styles.__login_input} name="username" type={'text'}placeholder="Username "onChange={handleInputChange}></input>
+                       <input className={styles.__login_input} name="email" type={'text'}placeholder="E-mail "onChange={handleInputChange}></input>
+                       <input className={styles.__login_input} name="password" type={'password'}placeholder="Contraseña "onChange={handleInputChange}></input>
+
+                       <div className={styles.__check_input}>
+                           <select className={styles.__check_input_option} onChange={(event) => setData({...data,role:event.target.value})}>
+                               <option value={'HOST'} className={styles.__check_input_select} >Host</option>
+                               <option value={'GUEST'} className={styles.__check_input_select} >Guest</option>
+                           </select>
+                       </div>
                        <Link to="/registerpage">
-                           <input className={styles.__login_input_button} type="button" value="Registrarse">
+                           <input className={styles.__login_input_button} type="button" value="Registrarse" onClick={SubmitForm}>
                            </input>
                        </Link>
                    </div>
