032235a6fd3c1106e13aacc327db10c632eeefc0
Isidro Escoda <adapptalo@gmail.com>
2020-09-15 18:41:23 +0200

    #59 RoomCard price, user and features)

--- frontend-app/src/components/listRoom/listRoom.view.js
+++ frontend-app/src/components/listRoom/listRoom.view.js
@@ -35,6 +35,8 @@ const ListRoom = () => {
                 return (
                     <RoomCard
                         name ={room.name}
+                        user ={room.user}
+                        features ={room.features}
                         description={room.description}
                         price={room.price}
                         key={room.id}
