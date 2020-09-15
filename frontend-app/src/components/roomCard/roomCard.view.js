a57f4b2d7d2e1d2e69b0266295354a5e2f5ff3ce
Isidro Escoda <adapptalo@gmail.com>
2020-09-15 19:00:19 +0200

    RoomCard price, user

--- frontend-app/src/components/roomCard/roomCard.view.js
+++ frontend-app/src/components/roomCard/roomCard.view.js
@@ -6,7 +6,7 @@ const RoomCard = ({name, description, price, user, features}) => {
     return (
       <div className={styles.__contenedorRoomCard}>
           <img className={styles.__galeria} src='https://www.metropoliabierta.com/uploads/s1/76/18/0/Foto%20Habitacio%CC%81n_5_570x340.jpeg' alt='Room in Barcelona'/>
-          <span className={styles.__nombre}>{user.username}<span className={styles.__features}>{features.length} features asociad@s</span></span>
+          <span className={styles.__nombre}>{user.username}</span>
 
           <span className={styles.__descripcion}>{description}</span>
           <span className={styles.__titulo}>{name}</span>
