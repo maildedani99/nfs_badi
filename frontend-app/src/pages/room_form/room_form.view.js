aa352ec291b3d13f2c35ab0e4f7ea4c488ce5b3c
dani <maildedani9@gmail.com>
2020-10-05 20:11:17 +0200

    segunda parte room form

--- frontend-app/src/pages/room_form/room_form.view.js
+++ frontend-app/src/pages/room_form/room_form.view.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import styles from './room_form.module.css';
 import room_img from './room_img.png';
+import fileicon from './fileicon.png';
 
 
 const RoomForm = () => {
 
     const user = JSON.parse(localStorage.getItem('user'));
     const [features, setFeatures] = useState([]);
+    const [next, setNext] = useState(true)
+
+    const nextClick = () => {
+        setNext(!next)
+    }
 
     const checkboxFetch = () => {
         const url = 'http://localhost/api/features';
@@ -39,12 +45,20 @@ const RoomForm = () => {
 
     const [checkedList, setCheckList] = useState({});
     var checkedListArray = []
+    var [filesArray, setFilesArray] = useState([]);
 
     const handleCheckBoxChange = (event) => {
         setCheckList({
             ...checkedList,
             [event.target.id]: event.target.checked,
         })
+    }
+    const handleFileChange = (event) => {
+        setFilesArray([
+            ...filesArray,
+            [event.target.name] =  event.target.files[0].name                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        ])
+        console.log(filesArray);
     }
 
     const selectTrue = () => {
@@ -61,6 +75,7 @@ const RoomForm = () => {
     }
 
     const submitForm = () => {
+        setNext(false);
         selectTrue();
         const url = 'http://localhost/api/rooms';
         const body = {
@@ -72,6 +87,8 @@ const RoomForm = () => {
             latitude: data.latitude,
             longitude: data.longitude,
             features: checkedListArray,
+            description: data.description,
+            images: filesArray
         };
         console.log(body);
 
@@ -87,7 +104,7 @@ const RoomForm = () => {
             .then(response => {
                 if (response.status === 201) {
                     console.log(response.status);
-                    alert("Tu habitación: \n"+ body.name +"\nse ha creado correctamente")
+                    alert("Tu habitación: \n" + body.name + "\nse ha creado correctamente")
                     return response.json();
                 }
                 return Promise.reject(response.status);
@@ -98,42 +115,146 @@ const RoomForm = () => {
     useEffect(() => {
         checkboxFetch()
     }, [])
-
     return (
 
         <div className={styles.__room_container}>
-            <div className={styles.__form_container}>
-                <h2>Comparte tu habitación en Room</h2>
-                <h4 className={styles.__from_subtitle}>Información básica</h4>
-                <div className={styles.__form_div}>
-                    <input className={styles.__input_habitacion} type="text" name="name" onChange={handleInputChange} placeholder="Nombre de la habitación" />
-                    <input className={styles.__input_compañeros} type="text" name="companions" onChange={handleInputChange} placeholder="Compañeros" />
-                    <input className={styles.__input_precio} type="text" name="price" onChange={handleInputChange} placeholder="Precio" />
-                </div>
-                <h4 className={styles.__from_subtitle}>Ubicación</h4>
-                <div className={styles.__ubicacion_div}>
-                    <input className={styles.__input_ubicacion} type="text" name="latitude" onChange={handleInputChange} placeholder="Latitud" />
-                    <input className={styles.__input_ubicacion} type="text" name="longitude" onChange={handleInputChange} placeholder="Longitud" />
+
+            { next ?
+
+                <div className={styles.__form_container}>
+                    <h2>Comparte tu habitación en Roomi</h2>
+                    <h4 className={styles.__from_subtitle}>Información básica</h4>
+                    <div>
+                        <div className={styles.__form_div}>
+                            <input className={styles.__input_habitacion} type="text" name="name" onChange={handleInputChange} placeholder="Nombre de la habitación" />
+                            <input className={styles.__input_compañeros} type="text" name="companions" onChange={handleInputChange} placeholder="Compañeros" />
+                            <input className={styles.__input_precio} type="text" name="price" onChange={handleInputChange} placeholder="Precio" />
+                        </div>
+                        <h4 className={styles.__from_subtitle}>Ubicación</h4>
+                        <div className={styles.__ubicacion_div}>
+                            <input className={styles.__input_ubicacion} type="text" name="latitude" onChange={handleInputChange} placeholder="Latitud" />
+                            <input className={styles.__input_ubicacion} type="text" name="longitude" onChange={handleInputChange} placeholder="Longitud" />
+                        </div>
+                    </div>
+
+                    <h4 className={styles.__from_subtitle}>Características</h4>
+                    <div className={styles.__div_checkbox}>
+                        {features.map((item) =>
+                            <div className={styles.__checkbox_group}>
+                                <input id={item.id} type="checkbox" className="check" onChange={handleCheckBoxChange} name={item.name} />
+                                <label className={styles.__room_label}> {item.name} </label>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.__div_button}>
+                        <input className={styles.__button_crear} type="button" onClick={nextClick} name="button" value="Siguiente" />
+                    </div>
                 </div>
 
-                <h4 className={styles.__from_subtitle}>Características</h4>
-                <div className={styles.__div_checkbox}>
-                    {features.map((item) =>
-                        <div className={styles.__checkbox_group}>
-                            <input id={item.id} type="checkbox" className="check" onChange={handleCheckBoxChange} name={item.name} />
-                            <label className={styles.__room_label}> {item.name} </label>
+                :
+
+                <div className={styles.__form_container}>
+                    <h2>Comparte tu habitación en Roomi</h2>
+                    <h4 className={styles.__from_subtitle}>Descripción</h4>
+                    <div>
+                        <div className={styles.__form_div}>
+                            <textarea className={styles.__textarea} name="description" onInput={handleInputChange} />
+
                         </div>
-                    )}
+                        <h4 className={styles.__from_subtitle}>Fotografías</h4>
+                        <div className={styles.__div_inputs_foto}>
+
+                            <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file1"  onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+
+                            <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file2"  onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+                            <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file3"  onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+
+                        </div>
+
+                        <div className={styles.__div_inputs_foto}>
+
+                        <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file4" onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+
+                            <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file5" onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+
+                            <div className={styles.__div_foto}>
+                                <label for="file-upload" className={styles.__label_foto}>
+                                    <img className={styles.__fileicon} src={fileicon} alt="icono" />
+                                    <p>Upload</p>
+                                </label>
+                                <input id="file-upload" name="file6" onInput={handleFileChange} type="file" style={{ display: "none" }} />
+                                <div id="info"> <p>Upload</p> </div>
+                            </div>
+
+
+
+
+                        </div>
+
+
+
+
+
+
+
+                    </div>
+
+                    <div className={styles.__div_button}>
+                        <input className={styles.__button_crear} type="submit" onClick={submitForm} name="button" value="Crear anuncio" />
+                    </div>
                 </div>
-                <div className={styles.__div_button}>
-                    <input className={styles.__button_crear} type="submit" onClick={submitForm} name="button" value="Crear anuncio" />
-                </div>
-            </div>
+
+            }
+
             <div className={styles.__div_img}>
                 <img src={room_img} alt="image" />
             </div>
-        </div>
 
+        </div>
     )
+
+
+    /* <input className={styles.__button_crear} type="submit" onClick={submitForm} name="button" value="Crear anuncio" /> */
+
 }
 export default RoomForm;
