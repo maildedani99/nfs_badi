50bfac43323597288822ae48e7099bb6e0e8a86c
MarcosIraola <iraolamarcos@gmail.com>
2020-10-15 15:02:27 +0200

    Context UploadPhoto

---
+++ frontend-app/src/contexts/uploadphoto_context.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+let UploadPhotoContext = React.createContext()
+let { Provider, Consumer } = UploadPhotoContext
+
+function UploadPhotoProvider({ children }) {
+
+    let [uploadPhotoArray, setUploadPhotoArray] = useState([])
+
+
+    return (
+        <Provider value={{ uploadPhotoArray, setUploadPhotoArray }}>
+            {children}
+        </Provider>
+    )
+}
+
+export { UploadPhotoProvider, Consumer as UploadPhotoConsumer, UploadPhotoContext }
