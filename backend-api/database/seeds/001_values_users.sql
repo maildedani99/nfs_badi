bfc00d1cde188b85c3eb1158e5c7e2025d1dbcdd
Facundo <facundo.emmanuel.herrera@gmail.com>
2020-10-23 18:34:55 +0200

    Seeds Values

---
+++ backend-api/database/seeds/001_values_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(id, email, username, first_name, last_name, gender, bio, birthday, role,
+email_verified_at, password, remember_token, created_at, updated_at)
+VALUES(1,'marcos@nds.com', 'Test1', 'Marcos','Rodriguez', 'M', 'Turismo','12-12-1980',
+'HOST',NULL, '12345',NULL,NULL,NULL),
+(2,'marcos@nds.com', 'Test1', 'Marcos','Rodriguez', 'M', 'Turismo','12-12-1980',
+'HOST',NULL, '12345',NULL,NULL,NULL)
---
+++ backend-api/database/seeds/002_values_rooms.sql
@@ -0,0 +1,3 @@
+INSERT INTO rooms (id, name, description, created_at, updated_at, user_id, latitude, longitude,
+companions, price, recommend)
+VALUES
