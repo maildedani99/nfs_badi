fea582caa713ab61fc2b61bd1c3ca3194cb2714d
Facundo <facundo.emmanuel.herrera@gmail.com>
2020-10-23 17:20:42 +0200

    new features

--- backend-api/database/seeds/004_values_features.sql
+++ backend-api/database/seeds/004_values_features.sql
@@ -1,2 +1,4 @@
 INSERT INTO "features" (id,name ,created_at,updated_at) VALUES (1,'WI-FI',NULL,NULL),(2,'Mascotas', NULL,NULL),(3,'Balcon',NULL, NULL),
 (4,'Exterior',NULL,NULL),(5,'Lavadora',NULL,NULL),(6,'Amueblada',NULL,NULL);
+
+
