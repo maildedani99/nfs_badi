166004512c1021d2fa6d46e5cb28d863550c4746
Facundo Herrera <facundo.emmanuel.herrera@gmail.com>
2020-10-28 17:58:16 +0100

    agregar companions a rooms

--- backend-api/database/seeds/002_values_rooms.sql
+++ backend-api/database/seeds/002_values_rooms.sql
@@ -1,3 +1,2 @@
-INSERT INTO rooms (id, name, description, created_at, updated_at, user_id, latitude, longitude,
-companions, price, recommend)
-VALUES
+ALTER TABLE rooms
+    ADD companions integer NULL
