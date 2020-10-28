21defd2f19b5b22c06674d8733b570d3c0be6277
Facundo Herrera <facundo.emmanuel.herrera@gmail.com>
2020-10-28 18:10:12 +0100

    Cambios class user y UserController

--- backend-api/database/scripts_psql/007_alter_rooms.sql
+++ backend-api/database/scripts_psql/007_alter_rooms.sql
@@ -1,2 +1,9 @@
+
+
 ALTER TABLE rooms
-    ADD companions integer NULL
+    ADD price integer NULL,
+    ADD latitude decimal(10,8) NOT NULL,
+    ADD longitude decimal(11,8)NOT NULL,
+    ADD recommend boolean NULL
+
+
